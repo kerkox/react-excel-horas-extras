7d0aa1d9eb5f40d28bf324d05258404ebb7d26d2
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-05-29 13:58:53 -0500

    URL del API

--- src/components/FormFile.js
+++ src/components/FormFile.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { config } from "../config/config";
+import "./FormFile.css";
+
 
 export const FormFile = () => {
+  const [archivos, setArchivos] = useState(null);
+
+  const API = config.prod.API_URL;
+  // const fileList = []
+
+  const subirArchivos = (e) => {
+    setArchivos(e.target.files);
+  };
+
+  const handleSubmit = async () => {
+    console.log("se llamo la funcion HandleSubmit")
+    const form = new FormData();
+    for(let index = 0; index < archivos.length; index++){
+      form.append(`files_${index}`,archivos[index])
+    }
+
+    await axios.post(`${API}/upload-excel`,form, {headers: {'Content-Type': 'multipart/form-data'}})
+    .then(response => {
+      console.log(response.data)
+      let code = response.data.data.code
+      window.open(`${API}/download-files/${code}`)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  };
+
   return (
-    <>
+    <div className="form-file">
       <p>Arrastra los archivos a subir</p>
-    </>
-  )
-}
+        <input
+          name="files_excel"
+          type="file"
+          multiple={true}
+          onChange={subirArchivos}
+        />
+        <button type="button" value="Subir" onClick={handleSubmit}>Subir</button>
+      
+    </div>
+  );
+};
---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  local: {
+    API_URL: "http://localhost:4000",
+  },
+  prod: {
+    API_URL: "https://api-excel-horas-extras.herokuapp.com",
+  },
+};
